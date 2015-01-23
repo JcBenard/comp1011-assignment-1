def596a5ce1f04634347fa2f79800c8fa447e343
JcBenard <niteburn@hotmail.com>
2015-01-22 20:55:39 -0500

    Created basic classes

---
+++ Comp1011-Assignmen 1/src/Hero.java
@@ -0,0 +1,7 @@
+/**
+ * @author Jean-Claude Benard
+ * Program Description: Create a hero object and its associated methods
+ */
+public class Hero {
+
+}
---
+++ Comp1011-Assignmen 1/src/HeroTester.java
@@ -0,0 +1,13 @@
+/**
+ * @author Jean-Claude Benard
+ * @version 1 - Created basic classes
+ * Program Description: Test the creation of the hero object and the assiociated methods
+ */
+public class HeroTester {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
