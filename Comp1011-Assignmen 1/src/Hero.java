0179a9beeb9b29e0112024f840a6581a49d2dbea
JcBenard <niteburn@hotmail.com>
2015-01-30 05:41:32 +0900

    Added more coments, now display what was rolled for hit and now roll 1 - 10 instead of 0 - 1.

--- Comp1011-Assignmen 1/src/Hero.java
+++ Comp1011-Assignmen 1/src/Hero.java
@@ -8,6 +8,7 @@ public class Hero {
 	private int strength;
 	private int speed;
 	private int health;
+	private double hitRoll;
 	
 	//public properties////////////////////////////////////////////////////////////////////////////
 	public String name;
@@ -20,36 +21,38 @@ public class Hero {
 	}
 	
 	//public methods///////////////////////////////////////////////////////////////////////////////
-	public boolean fight(){
+	public boolean fight(){//make the hero fight
 		if(hitAttempt() == true){
-			System.out.println(this.name + " has hit the opponent dealing " + hitDamage() + " damage.");
+			System.out.println(this.name + " has hit the opponent dealing " + hitDamage() + " damage. They rolled a " + hitRoll + " out of 1.");
 			return true;
 		}else{
-			System.out.println(this.name + " has missed the opponent.");
+			System.out.println(this.name + " has missed the opponent. They rolled a " + hitRoll + " out of 1.");
 			return false;
 		}
 	}
 	
-	public void show(){
+	public void show(){//show the heros stats
 		System.out.println("Name: " + this.name + "\nStrength: " + this.strength + "\nSpeed: " + this.speed + "\nHealth: " + this.health);
 	}
 	
 	//private methods////////////////////////////////////////////////////////////////////////////
-	private void generateAbilities(){
+	private void generateAbilities(){//generate the ablities of the hero
 		this.strength = (int)(Math.random() * 100) + 1;
 		this.speed = (int)(Math.random() * 100) + 1;
 		this.health = (int)(Math.random() * 100) + 1;
 	}
 		
-	private boolean hitAttempt(){
-		if(Math.random() >= .8){
+	private boolean hitAttempt(){//make the hero attempt to hit		
+		
+		hitRoll = Math.floor((Math.random() * 10) +1) ;
+		if(hitRoll > 8){
 			return true;
 		}else{
 			return false;
 		}
 	}
 	
-	private int hitDamage(){
+	private int hitDamage(){//calculate how much damage the hero does
 		return ((int)(Math.random() * 6) + 1) * this.strength;
 	}
 }
